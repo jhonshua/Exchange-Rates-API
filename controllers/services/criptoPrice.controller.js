a21210eaaea6346ef8eb6f34b2e3f9b6bac5a36f
Julio-PC\Julio <julio.llinas@gmail.com>
2024-06-27 17:09:29 -0400

    creo la ruta get price cripto

--- controllers/services/criptoPrice.controller.js
+++ controllers/services/criptoPrice.controller.js
@@ -1,6 +1,6 @@
 import CryptoData from '../../models/cryptoData.model.js';
 
-export const getPriceCripto = async (req, res) => {
+export const GetPriceCripto = async (req, res) => {
     try {
         // Ensure sorting by fecha with optional secondary sorting by _id
         const Values = await CryptoData.find();
@@ -9,11 +9,10 @@ export const getPriceCripto = async (req, res) => {
         if (Values.length === 0) {
             return res.status(404).json({ message: 'No se encontraron registros de Criptos' });
         }
-
-
-        return res.status(200).json({ data: Values });
+        return res.status(200).json({data: Values });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Error interno del servidor' });
     }
 };
+
--- routes/services.routes.js
+++ routes/services.routes.js
@@ -4,12 +4,15 @@ import { getPriceUsd } from '../controllers/services/usdPrice.controller.js';
 import { getPriceRub } from '../controllers/services/rubPrice.controller.js';
 import { getPriceEur } from '../controllers/services/eurPrice.controller.js';
 import { GetPrecioNational } from '../controllers/services/sistemaNationalPrice.controller.js';
+import { GetPriceCripto } from '../controllers/services/criptoPrice.controller.js'
 
 const router = Router();
 
-router.get('/GetPrecioUsd/:apiKey',  verifyApiKey, getPriceUsd );
-router.get('/GetPrecioRub/:apiKey',  verifyApiKey, getPriceRub );
-router.get('/GetPrecioEur/:apiKey',  verifyApiKey, getPriceEur );
-router.get('/GetPrecioNational/:apiKey',  verifyApiKey, GetPrecioNational );
+router.get('/GetPrecioUsd/:apiKey', verifyApiKey, getPriceUsd);
+router.get('/GetPrecioRub/:apiKey', verifyApiKey, getPriceRub);
+router.get('/GetPrecioEur/:apiKey', verifyApiKey, getPriceEur);
+router.get('/GetPrecioNational/:apiKey', verifyApiKey, GetPrecioNational);
+router.get('/GetCripto/:apikey', verifyApiKey,  GetPriceCripto);
+
 
 export default router;
